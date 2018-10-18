54913b1e795417203220e47c489915b31c00cf9e
nikolkate <nikolkate92@yandex.ru>
2018-10-18 10:03:35 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,7 +4,25 @@ public class MyFirstProgram {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        hello("world");
+        hello("user");
+        hello("Kate");
+
+        Square s = new Square(5);
+
+        System.out.println("Площадь квадрата " + s.l + " = " + s.area());
+
+        Rectangle r = new Rectangle(4, 5);
+
+        System.out.println("Площадь прямоугольница со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
     }
+
+    public static void hello (String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+
+
+
 }
